27d13b247e6cb599c5fe57a03a7f4631dfb97439
OmarJouied <omarjouied1421@yahoo.com>
2026-05-01 19:19:44 +0100

    lesson for literals and escape sequences

---
+++ Literals&EscapeSequences/Literals&EscapeSequences.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "M1\\M2\\M3 \n";
+	cout << "M1\tM2\tM3 \n";
+	cout << "My name is \" Omar \" ";
+	cout << "\a";
+
+	return 0;
+}
