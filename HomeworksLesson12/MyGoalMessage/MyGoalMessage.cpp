e1a152500f8e6121420783f0249d07e3cdff3df5
OmarJouied <omarjouied1421@yahoo.com>
2026-05-01 08:39:30 +0100

    finishing the first homework about printing in c++

---
+++ HomeworksLesson12/MyGoalMessage/MyGoalMessage.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I Love Programming!\n\n";
+    std::cout << "I promise to be the best developer ever!\n\n";
+    std::cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
+    std::cout << "Best Regards,\n";
+    std::cout << "Omar Jouied.\n";
+
+    return 0;
+}
---
+++ HomeworksLesson12/PersonalInfo/PersonalInfo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "*************************\n";
+
+    std::cout << "Name: " << "Omar Jouied.\n";
+    std::cout << "Age : " << 26 << " Years.\n";
+    std::cout << "City: " << "Mdiq.\n";
+    std::cout << "Country: " << "Morocco.\n";
+    
+    std::cout << "*************************\n";
+
+    return 0;
+}
