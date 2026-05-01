804d18fb7c3ad8f150338d94248a0033c31a6502
OmarJouied <omarjouied1421@yahoo.com>
2026-05-01 18:51:36 +0100

    make some comments in c++, single- lines and multi-lignes

---
+++ Comments/Comments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    // this code for printing my name. this single line comment
+
+    /*
+    * you can leave a comment in multi-line like this.
+    */
+    std::cout << "Omar Jouied\n";
+
+    return 0;
+}
