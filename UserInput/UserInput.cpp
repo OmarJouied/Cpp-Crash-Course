ca532573e1a4c1ac89fe278c1eb69418b19a7a93
OmarJouied <omarjouied1421@yahoo.com>
2026-05-15 18:20:03 +0100

    learn in lesson 18 how to read data from user using std::cin

---
+++ UserInput/UserInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char MyChar;
+    int MyNumber;
+
+    cout << "Please enter a char?" << endl;
+    cin >> MyChar;
+
+    cout << "Please enter a Number?" << endl;
+    cin >> MyNumber;
+
+    cout << "You entered char: " << MyChar << " and Number: " << MyNumber << endl;
+
+    return 0;
+}
