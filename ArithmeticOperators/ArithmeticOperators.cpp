777b48a2312897f09a44b8694dc18a56f4f9d980
OmarJouied <omarjouied1421@yahoo.com>
2026-05-16 07:14:01 +0100

    lesson 20 Arithmetic Operators

---
+++ ArithmeticOperators/ArithmeticOperators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A = 10, B = 20;
+
+	cout << "A + B = " << A + B << endl;
+	cout << "A - B = " << A - B << endl;
+	cout << "B - A = " << B - A << endl;
+	cout << "A * B = " << A * B << endl;
+	cout << "A / B = " << A / B << endl;
+	cout << "B / A = " << B / A << endl;
+	cout << "A % B = " << A % B << endl;
+	cout << "B % A = " << B % A << endl;
+
+	return 0;
+}
