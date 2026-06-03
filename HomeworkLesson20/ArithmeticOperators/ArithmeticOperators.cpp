e3dbb41555a00bdd7cf44940d8144c7ce92a9398
OmarJouied <omarjouied1421@yahoo.com>
2026-06-03 23:35:35 +0100

    homeword 20 about arithmetic operators

---
+++ HomeworkLesson20/ArithmeticOperators/ArithmeticOperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+
+    cout << "Please enter the first Number A?\n";
+    cin >> A;
+    cout << "Please enter the first Number B?\n";
+    cin >> B;
+
+    cout << endl;
+
+    cout << A << " + " << B << " = " << A + B << endl;
+    cout << A << " - " << B << " = " << A - B << endl;
+    cout << A << " * " << B << " = " << A * B << endl;
+    cout << A << " / " << B << " = " << A / B << endl;
+    cout << A << " % " << B << " = " << A % B << endl;
+    return 0;
+}
