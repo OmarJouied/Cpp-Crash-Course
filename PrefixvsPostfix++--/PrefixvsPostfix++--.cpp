8bfa2b3152df06d2821f25e7b4820ad148d94c9b
OmarJouied <omarjouied1421@yahoo.com>
2026-06-05 23:27:59 +0100

    Prefix vs Postfix in ++ -- operators lesson 22

---
+++ PrefixvsPostfix++--/PrefixvsPostfix++--.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 10, B = A++;
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+
+    B = ++A;
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+
+    B = --A;
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+
+    return 0;
+}
