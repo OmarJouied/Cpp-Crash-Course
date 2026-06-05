2889d149e67c5856b8520548be806ba64e928bfd
OmarJouied <omarjouied1421@yahoo.com>
2026-06-05 21:59:16 +0100

    Increment and Decrement operators lesson 21

---
+++ Increment&DecrementOperators/Increment&DecrementOperators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 10, B = 20;
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+
+    A++; // you can use ++A as well
+    B--; // you can use --B as well
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+
+    return 0;
+}
