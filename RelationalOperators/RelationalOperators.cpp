512f0c11b17ed8199487532e55a77383d4b1787c
OmarJouied <omarjouied1421@yahoo.com>
2026-06-06 16:02:00 +0100

    Relational Operators Lesson 24

---
+++ RelationalOperators/RelationalOperators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 10, B = 20;
+
+    cout << "A = " << A << ", B = " << B << endl << endl;
+
+    cout << "A == B = " << (A == B) << endl;
+    cout << "A != B = " << (A != B) << endl;
+    cout << "A > B = " << (A > B) << endl;
+    cout << "A < B = " << (A < B) << endl;
+    cout << "A >= B = " << (A >= B) << endl;
+    cout << "A <= B = " << (A <= B) << endl;
+
+    return 0;
+}
