7ecdcf6e22ea141838c7af5b8edd99bb2f1561e5
OmarJouied <omarjouied1421@yahoo.com>
2026-08-21 18:31:33 +0100

    Lesson 36 Variable Scope Local VS Global Variables

---
+++ VariableScopeGlobalVsLocal/VariableScopeGlobalVsLocal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int x = 300;
+
+void MyFunction1()
+{
+    int x = 500;
+
+    cout << "The value of x inside function is: " << x << endl;
+}
+
+int main()
+{
+    int x = 10;
+
+    cout << "The local value of x inside Main is: " << x << endl;
+    MyFunction1();
+
+    cout << "The global value of x is: " << ::x << endl;
+    
+    ::x++;
+
+    cout << "The global value of x is: " << ::x << endl;
+    return 0;
+}
