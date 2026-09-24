eaefae81ef64cbbfdfc957593c72123df1856208
OmarJouied <omarjouied1421@yahoo.com>
2026-09-24 14:06:23 +0100

    Lesson 40 - Arrays with Functions

---
+++ ArraysWithFunctions/ArraysWithFunctions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void ReadArrayData(int x[3])
+{
+    cout << "Please enter number1?\n";
+    cin >> x[0];
+
+    cout << "Please enter number2?\n";
+    cin >> x[1];
+
+    cout << "Please enter number3?\n";
+    cin >> x[2];
+}
+
+void PrintArrayData(int x[3])
+{
+    cout << "**************************\n";
+
+    cout << x[0] << endl;
+    cout << x[1] << endl;
+    cout << x[2] << endl;
+}
+
+int main()
+{
+    int x[3];
+
+    ReadArrayData(x);
+    PrintArrayData(x);
+
+    return 0;
+}
