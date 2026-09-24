fdbbb6ed7a03192813f0a74f7c48097f84f81bb8
OmarJouied <omarjouied1421@yahoo.com>
2026-09-24 15:34:58 +0100

    Homework of Lesson 40 about arrays with functions

--- HomeworkLesson39/HomeworkLesson39.cpp
+++ HomeworkLesson39/HomeworkLesson39.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void ReadGrades(float Grades[3])
 {
-    float Grades[3];
-
     cout << "Please Enter Grade 1?\n";
     cin >> Grades[0];
 
@@ -13,9 +11,28 @@ int main()
 
     cout << "Please Enter Grade 3?\n";
     cin >> Grades[2];
+}
 
+float CalculateAverage(float Grades[3])
+{
+    return (Grades[0] + Grades[1] + Grades[2]) / 3;
+}
+
+void PrintAverage(float Average)
+{
     cout << "*********************\n";
-    cout << "The average of grades is " << (Grades[0] + Grades[1] + Grades[2]) / 3 << endl;
+    cout << "The average of grades is " << Average << endl;
+}
+
+int main()
+{
+    float Grades[3], Average;
+
+    ReadGrades(Grades);
+
+    Average = CalculateAverage(Grades);
+
+    PrintAverage(Average);
 
     return 0;
 }
